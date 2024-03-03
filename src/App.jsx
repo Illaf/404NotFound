525b26d3aec81a3946fb6ca325fd67898693551d
Illaf <ilafshafeeq@gmail.com>
2024-03-03 16:06:24 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import {Route,Link} from "react-router-dom";
+import {Layout,Typography,Space} from 'antd';
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='app'>
+        <div className='navbar'>
+
+        </div>
+        <div className='main'>
+
+</div>
+<div className='footer'>
+
+</div>
+      </div>
+    </>
+  )
+}
+
+export default App
